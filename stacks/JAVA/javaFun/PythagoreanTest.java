d6069d68d35e93371fe18153962757c05c20a4ca
timfoltz <73617242+timfoltz@users.noreply.github.com>
2021-02-08 11:51:04 -0700

    completed a few assignments
    pushing assignments

---
+++ stacks/JAVA/javaFun/PythagoreanTest.java
@@ -0,0 +1,8 @@
+public class PythagoreanTest {
+    public static void main(String[] args){
+        Pythagorean iD = new Pythagorean();
+        Double result = iD.calculateHypotenuse(15, 25);
+        System.out.println("The hypotenuse is: "+result);
+
+    }
+}
