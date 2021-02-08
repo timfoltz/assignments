4f79f8939168acaac16790a91136ee6661ada939
timfoltz <73617242+timfoltz@users.noreply.github.com>
2021-02-08 12:30:18 -0700

    FizzBuzz Assignment
    finished FizzBuzz

---
+++ stacks/JAVA/javaFun/FizzBuzzTest.java
@@ -0,0 +1,7 @@
+public class FizzBuzzTest {
+    public static void main(String[] args){
+        FizzBuzz fizz = new FizzBuzz();
+        String result = fizz.fizzBuzz(2);
+        System.out.println(result);
+    }
+}
