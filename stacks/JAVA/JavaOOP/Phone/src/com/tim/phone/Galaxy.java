a700e0d513ab1e1101ca85fadfb54be7cfb03c60
timfoltz <73617242+timfoltz@users.noreply.github.com>
2021-02-11 19:38:40 -0700

    Completed Phone assignment

---
+++ stacks/JAVA/JavaOOP/Phone/src/com/tim/phone/Galaxy.java
@@ -0,0 +1,29 @@
+package com.tim.phone;
+
+public class Galaxy extends Phone implements Ringable {
+
+	public Galaxy(String versionNumber, int batteryPercentage, String carrier, String ringTone) {
+		super(versionNumber, batteryPercentage, carrier, ringTone);
+		
+	}
+
+	@Override
+	public String ring() {
+		return this.getRingTone();
+	}
+
+	@Override
+	public String unlock() {
+		return "You've got mail";
+	}
+
+	@Override
+	public void displayInfo() {
+		System.out.println("versionNumber: " + this.getVersionNumber() +
+				", batteryPercentage: " + this.getBatteryPercentage() +
+				", carrier: " + this.getCarrier() +
+				", ringTone: " + this.getRingTone());
+
+	}
+
+}
---
+++ stacks/JAVA/JavaOOP/Phone/src/com/tim/phone/Ringable.java
@@ -0,0 +1,6 @@
+package com.tim.phone;
+
+public interface Ringable {
+	public String ring();
+	public String unlock();
+}
